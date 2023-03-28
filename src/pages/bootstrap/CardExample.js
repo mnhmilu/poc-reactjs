ccb8395610591ba142a232a09aebe8303f72fb89
Md. Nahid Hossain <mnh_milu@yahoo.com>
2023-03-28 14:09:21 +0600

    update for dynamic data
    dynamic data using fetch and useEffect

--- src/pages/bootstrap/CardExample.js
+++ src/pages/bootstrap/CardExample.js
@@ -1,20 +1,41 @@
+import React, { useState, useEffect, Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Holder from 'react-holder'
+import Holder from 'react-holder';
 
 function CardExample() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+           <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>
+                  {item.body}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
