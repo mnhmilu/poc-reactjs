96d68e8d88d828ba98a69dccf4513abd8e56ec89
Md. Nahid Hossain <mnh_milu@yahoo.com>
2023-03-28 14:07:47 +0600

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,14 @@
 import CardExample from "./bootstrap/CardExample";
 
-const Contact = () => {
-  return (
-    <div>
-      <h1>Contact Me</h1>
-      <CardExample />
-    </div>
-  )
+  const Contact = () => {
 
-};
+    return (
+      <div>
+        <h1>Contact Me </h1>
+        <CardExample />
+      </div>
+    )  
+  };
+
 
 export default Contact;
